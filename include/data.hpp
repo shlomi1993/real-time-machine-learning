50598d74e3ba7ab21a976686fe38fc90fc41b09e
Shlomi Ben Shushan <shlomiggl@gmail.com>
2024-10-12 21:45:40 +0300

    Improve ETL

--- include/data.hpp
+++ include/data.hpp
@@ -1,32 +1,29 @@
-#ifndef __DATA_H
-#define __DATA_H
+#ifndef DATA_HPP
+#define DATA_HPP
 
 #include <vector>
-#include "stdint.h"
-#include "stdio.h"
+#include <cstdint>
+#include <memory>
 
 class Data {
-    std::vector<uint8_t> *m_feature_vector; // No class at end.
+    std::vector<uint8_t> m_feature_vector;
     uint8_t m_label;
     int m_enum_label; // A -> 1, B -> 2
     double m_distance;
 
-  public:
+public:
     Data();
-    ~Data() {}
-    void set_feature_vector(std::vector<uint8_t> *);
-    void append_to_feature_vector(uint8_t);
-    void set_label(uint8_t);
-    void set_enumerated_label(int);
-
-    int get_feature_vector_size();
-    uint8_t get_label();
-    uint8_t get_enumerated_label();
+    void set_feature_vector(const std::vector<uint8_t>& feature_vector);
+    void append_to_feature_vector(uint8_t value);
+    void set_label(uint8_t label);
+    void set_enumerated_label(int enum_label);
     void set_distance(double distance);
 
-    std::vector<uint8_t> *get_feature_vector();
-    double get_distance();
+    int get_feature_vector_size() const;
+    uint8_t get_label() const;
+    int get_enumerated_label() const;
+    const std::vector<uint8_t>& get_feature_vector() const;
+    double get_distance() const;
 };
 
-
-#endif
+#endif // DATA_HPP
