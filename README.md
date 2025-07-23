fc73a053aba09532f284dba81f2d82fbeaebf641
Shlomi Ben Shushan <shlomiggl@gmail.com>
2025-07-23 22:19:38 +0300

    update

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# Work In Progress
-
 # Machine Learning in C++
+
 Implementation and code examples of machine learning algorithms in C++. Includes ETL (Extract, Transform, Load), KNN (supervised learning), K-Means (unsupervised learning), and neural networks.
 
-TDL:
-- Complete
-- Documentation
+# Work In Progress...
+
+## To-do
+- Fix KNN - it is compiled and run, but low performance...
+- Complete neural networks
 - LI ?
 - 1-Cher-a-Day + implement advanced part
 - - RPI5 C++ code examples
--- models/ann/include/network.hpp
+++ models/ann/include/network.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
-#include "common.hpp"
-#include "data.hpp"
+#include "data_set.hpp"
+#include "data_point.hpp"
 #include "neuron.hpp"
 #include "layer.hpp"
 
-class Network : public CommonData {
+class Network : public DataSet {
     std::vector<Layer *> layers;
     double learning_rate;
     double test_performance;
@@ -15,10 +15,10 @@ public:
     double activate(std::vector<double> weights, std::vector<double> inputs);  // Dot product
     double inline transfer(double);
     double inline transfer_derivative(double);  // Used for Back-Propagation
-    std::vector<double> fprop(Data *data);
-    void bprop(Data *data);
-    void update_weights(Data *data);
-    int predict(Data *data);
+    std::vector<double> fprop(DataPoint *data);
+    void bprop(DataPoint *data);
+    void update_weights(DataPoint *data);
+    int predict(DataPoint *data);
     void train(int n_iterations);
     double validate();
     double test();
